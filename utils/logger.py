01c9bf8b4a9a3581891712284fa5a7cb634c5b15
Abir Arafat Chawdhury <abirxdhackz.info.me@gmail.com>
2026-01-31 00:29:13 +0600

    Create logger.py

---
+++ utils/logger.py
@@ -0,0 +1,23 @@
+#Copyright @ISmartCoder
+#Updates Channel @abirxdhackz
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+for logger_name in (
+    "telethon",
+    "telethon.client",
+    "telethon.network",
+    "telethon.extensions",
+    "telethon.sessions",
+):
+    logging.getLogger(logger_name).setLevel(logging.ERROR)
+
+LOGGER = logging.getLogger(__name__)
+
+LOGGER.info("Creating Logger For Logging...")
+LOGGER.info("Logger Successfully Created & Initialized")
